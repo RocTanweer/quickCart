544fc70eae70e25203c33191e64b113b4d7ff897
Roc Tanweer <mohammadtanweer769@gmail.com>
2024-02-04 23:37:39 +0530

    ADD ➕: implementation of getUserData

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,9 +1,24 @@
-export const getUserProfile = async (req, res) => {
+import { getUserById } from "../database/userQueries.js";
+
+export const getUserData = async (req, res) => {
   try {
-  } catch (error) {}
+    const userId = parseInt(req.params.userId);
+
+    const userData = await getUserById(userId);
+
+    if (!userData) {
+      res.status(404);
+      throw new Error("User not found");
+    }
+
+    delete userData.password;
+    res.status(200).json(userData);
+  } catch (error) {
+    res.json({ message: error.message });
+  }
 };
 
-export const patchUserProfile = async (req, res) => {
+export const updateUserData = async (req, res) => {
   try {
   } catch (error) {}
 };
