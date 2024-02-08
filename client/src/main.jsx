16a76e68f21b427fb4c1be10b1b19f75909590a2
Roc Tanweer <mohammadtanweer769@gmail.com>
2024-02-09 00:27:47 +0530

    MOD 🖊️: Added GlobalContextProvider to main.jsx
    MyRouterProvider is replaced with global level
    context provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import MyRouterProvider from "./context/routerProvider/MyRouterProvider";
+import GlobalContextProvider from "./context/GlobalContextProvider";
 
 import CssBaseline from "@mui/material/CssBaseline";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <CssBaseline />
-    <MyRouterProvider />
+    <GlobalContextProvider />
   </React.StrictMode>
 );
