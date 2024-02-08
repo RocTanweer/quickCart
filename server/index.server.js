3729e4b4bde2cc968b5e824b8ec5cae23c9180b8
Roc Tanweer <mohammadtanweer769@gmail.com>
2024-02-09 00:04:20 +0530

    ADD ➕: cors in the main server file
    For cross-origin request, origin is added
    To expose cookie for client-side-script, credentials is added

--- server/index.server.js
+++ server/index.server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import { SERVER_PORT } from "./config/envVar.js";
 
@@ -9,6 +10,12 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api", combinedRoutes());
 
