d30b374673f06a81b55ffd36613f441c3ca13382
Roc Tanweer <mohammadtanweer769@gmail.com>
2024-02-10 03:22:20 +0530

    ADD ➕: Wrapper component to protect routes
    It is a simple implementation of RBAS client-side
    routing

---
+++ client/src/components/GuardRoute.jsx
@@ -0,0 +1,32 @@
+import { Outlet, Navigate, useLocation } from "react-router-dom";
+
+import { useSelector } from "react-redux";
+
+import { userRoleSelector } from "../state/slices/loginSlice";
+
+const GuardRoute = ({ role }) => {
+  const userRole = useSelector(userRoleSelector);
+  const location = useLocation();
+
+  let isAuthorize = false;
+  if (role === "ADMIN") {
+    isAuthorize = userRole === "ADMIN";
+  } else if (role === "USER") {
+    isAuthorize = userRole === "USER" || userRole === "ADMIN";
+  }
+  return (
+    <>
+      {isAuthorize ? (
+        <Outlet />
+      ) : (
+        <Navigate
+          to={"/login"}
+          replace={true}
+          state={{ prevPath: location.pathname }}
+        />
+      )}
+    </>
+  );
+};
+
+export default GuardRoute;
