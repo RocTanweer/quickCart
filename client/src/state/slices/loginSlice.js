c3c6150eb82c1b3b246eee10aecc69868175e215
Roc Tanweer <mohammadtanweer769@gmail.com>
2024-02-10 03:14:57 +0530

    MOD 🖊️: init state of loginSlice
    Additionally, Added new reducer to update the new
    state and exported its action creator function
    and a selector for extracting userRole

--- client/src/state/slices/loginSlice.js
+++ client/src/state/slices/loginSlice.js
@@ -1,28 +1,35 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import Cookies from "js-cookie";
+import { jwtDecode } from "jwt-decode";
 
 import { axCli } from "../../lib/axiosClient";
 
 export const login = createAsyncThunk("login/submit", async (userData) => {
   const response = await axCli.post("/api/login", userData);
-  return response.data.userData;
+  return response.data;
 });
 
 export const loginSlice = createSlice({
   name: "login",
   initialState: {
-    data: null,
     status: "idle",
     error: null,
+    userLoginInfo: Cookies.get("qcticket")
+      ? jwtDecode(JSON.parse(Cookies.get("qcticket")).token)
+      : {},
   },
-  reducers: {},
+  reducers: {
+    setUserLoginInfo(state, action) {
+      state.userLoginInfo = action.payload;
+    },
+  },
   extraReducers: (builder) => {
     builder
       .addCase(login.pending, (state) => {
         state.status = "loading";
       })
-      .addCase(login.fulfilled, (state, action) => {
+      .addCase(login.fulfilled, (state) => {
         state.status = "succeeded";
-        state.data = action.payload;
       })
       .addCase(login.rejected, (state, action) => {
         state.status = "failed";
@@ -30,5 +37,9 @@ export const loginSlice = createSlice({
       });
   },
 });
+
+export const userRoleSelector = (state) => state.login.userLoginInfo?.userRole;
+
+export const { setUserLoginInfo } = loginSlice.actions;
 
 export default loginSlice.reducer;
