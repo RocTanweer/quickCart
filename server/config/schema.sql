d574fad195c850a70a42b86e22a550653783f636
Roc Tanweer <mohammadtanweer769@gmail.com>
2024-02-12 21:49:05 +0530

    ADD ➕: sql script to create tables
    I added sql script to create tables: product category,
    product brand, and product

--- server/config/schema.sql
+++ server/config/schema.sql
@@ -15,3 +15,35 @@ CREATE TABLE IF NOT EXISTS `user` (
     PRIMARY KEY (`user_id`),
     UNIQUE KEY `email_UNIQUE` (`email`)
 );
+
+-- Create product category table 
+CREATE TABLE IF NOT EXISTS `product_category` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `thumbnail` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- Create product brand table
+CREATE TABLE IF NOT EXISTS `product_brand` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- Create product table
+CREATE TABLE IF NOT EXISTS `product` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `product_category_id` INT NOT NULL,
+  `product_brand_id` INT NOT NULL,
+  `unit_price` DECIMAL(9,2) NOT NULL,
+  `stock_quantity` INT NOT NULL DEFAULT '0',
+  `name` VARCHAR(150) NOT NULL,
+  `description` VARCHAR(500) NOT NULL,
+  `image` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_product_category_id_idx` (`product_category_id`),
+  KEY `fk_product_brand_id_idx` (`product_brand_id`),
+  CONSTRAINT `fk_product_brand_id` FOREIGN KEY (`product_brand_id`) REFERENCES `product_brand` (`id`) ON DELETE RESTRICT,
+  CONSTRAINT `fk_product_category_id` FOREIGN KEY (`product_category_id`) REFERENCES `product_category` (`id`) ON DELETE RESTRICT
+);
