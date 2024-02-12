80e7ef0163a74a8aa98cf6f470e68c32d4d35bb1
Roc Tanweer <mohammadtanweer769@gmail.com>
2024-02-13 03:45:19 +0530

    ADD ➕: product routes in combinedRoutes

--- server/routes/combinedRoutes.js
+++ server/routes/combinedRoutes.js
@@ -2,11 +2,13 @@ import express from "express";
 
 import createAuthRoutes from "./authRoutes.js";
 import createUserRoutes from "./userRoutes.js";
+import createProductRoutes from "./productRoutes.js";
 
 export const router = express.Router();
 
 export default () => {
   createAuthRoutes(router);
   createUserRoutes(router);
+  createProductRoutes(router);
   return router;
 };
