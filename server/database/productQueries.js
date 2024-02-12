ce66c70187316558d03c52fb0a50293f7aa5496b
Roc Tanweer <mohammadtanweer769@gmail.com>
2024-02-12 23:03:45 +0530

    ADD ➕: productQueries for basic CRUD op
    These functions are not tested yet.
    They will be tested later when ui is developed
    I will then refactor them if needed

--- server/database/productQueries.js
+++ server/database/productQueries.js
@@ -1,35 +1,130 @@
 import { connection } from "../config/database.js";
 
-export const getProducts = async (conditions) => {
-  const { page, pageSize, category, brand, minPrice, maxPrice, availability } =
-    conditions;
-
-  let sql = "SELECT * FROM product WHERE 1=1";
-  const values = [];
-
-  if (category) {
-    sql += " AND category = ?";
-    values.push(category);
-  }
-  if (brand) {
-    sql += " AND brand = ?";
-    values.push(brand);
-  }
-  if (availability) sql += ` AND stock_quantity > 0`;
-
-  if (minPrice && maxPrice) {
-    (sql += " AND price >= ? AND price <= ?"),
-      values.push(parseFloat(minPrice), parseFloat(maxPrice));
-  }
-
-  const offset = (parseInt(page) - 1) * parseInt(pageSize);
-
-  sql += ` LIMIT ?, ?`;
-  values.push(offset, parseInt(pageSize));
-
+export const createProduct = async (productDetails) => {
   try {
-    const results = await connection.execute(sql, values);
-    console.log(results);
+    const {
+      productCategoryId,
+      productBrandId,
+      unitPrice,
+      stockQuantity,
+      name,
+      description,
+      image,
+    } = productDetails;
+
+    const sql = `INSERT INTO product
+       (product_category_id, product_brand_id, unit_price, stock_quantity, name, description, image)
+       VALUES (?, ?, ?, ?, ?, ?, ?)
+       `;
+    const values = [
+      productCategoryId,
+      productBrandId,
+      unitPrice,
+      stockQuantity,
+      name,
+      description,
+      image,
+    ];
+
+    const [result] = await connection.execute(sql, values);
+    return result[0];
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getProducts = async (conditions) => {
+  try {
+    const {
+      page,
+      pageSize,
+      category,
+      brand,
+      minPrice,
+      maxPrice,
+      availability,
+    } = conditions;
+
+    // later fetch only columns that is required in product list
+    let sql = "SELECT id, unit_price, name, image FROM product WHERE 1=1";
+    const values = [];
+
+    if (category) {
+      sql += " AND product_category_id = ?";
+      values.push(category);
+    }
+    if (brand) {
+      sql += " AND product_brand_id = ?";
+      values.push(brand);
+    }
+    if (availability) sql += ` AND stock_quantity > 0`;
+
+    if (minPrice && maxPrice) {
+      (sql += " AND unit_price >= ? AND unit_price <= ?"),
+        values.push(parseFloat(minPrice), parseFloat(maxPrice));
+    }
+
+    const offset = (parseInt(page) - 1) * parseInt(pageSize);
+
+    sql += ` LIMIT ?, ?`;
+    values.push(offset, parseInt(pageSize));
+
+    const [results] = await connection.execute(sql, values);
+    return results;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getProductById = async (productId) => {
+  try {
+    const sql = "SELECT * FROM product WHERE id = ?";
+    const value = [productId];
+
+    const [result] = await connection.execute(sql, value);
+    return result[0];
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const updateProduct = async (productId, productUpdates) => {
+  try {
+    const { unitPrice, stockQuantity, name, description, image } =
+      productUpdates;
+
+    const sql = `UPDATE product
+    SET
+    unit_price = COALESCE(?, unit_price),
+    stock_quantity = COALESCE(?, stock_quantity),
+    name = COALESCE(?, name),
+    description = COALESCE(?, description),
+    image = COALESCE(?, image)
+    WHERE id = ?
+    `;
+    const values = [
+      unitPrice,
+      stockQuantity,
+      name,
+      description,
+      image,
+      productId,
+    ];
+
+    const [result] = await connection.execute(sql, values);
+    return result[0];
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const deleteProduct = async (productId) => {
+  try {
+    const sql = "DELETE FROM product WHERE id = ?";
+    const value = [productId];
+
+    const [result] = await connection.execute(sql, value);
+    return result[0];
   } catch (error) {
     throw error;
   }
