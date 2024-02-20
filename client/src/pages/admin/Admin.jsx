090bee5f59e0201a78aadb523058973b9d4ee9d5
Roc Tanweer <mohammadtanweer769@gmail.com>
2024-02-20 23:51:02 +0530

    ADD ➕: basic admin page
    This includes Tabs component that works expectedly.
    Also includes basic implementation of Users layout
    section

--- client/src/pages/admin/Admin.jsx
+++ client/src/pages/admin/Admin.jsx
@@ -1,5 +1,32 @@
+import { useState } from "react";
+
+import { FlexBox } from "../../layouts";
+
+import {
+  Users,
+  ProductList,
+  ProductCreate,
+  ProductCategory,
+  ProductBrand,
+  Orders,
+  Payments,
+  Tabs,
+} from "./layouts";
+
+import tabNames from "./constants/tabNames";
+
 const Admin = () => {
-  return <div>Admin</div>;
+  const [selectedSec, setSelectedSec] = useState(tabNames.users);
+
+  return (
+    <div>
+      <Tabs activeSec={selectedSec} handleActiveSec={setSelectedSec} />
+
+      <FlexBox sx={{ paddingX: "24px" }}>
+        <Users />
+      </FlexBox>
+    </div>
+  );
 };
 
 export default Admin;
