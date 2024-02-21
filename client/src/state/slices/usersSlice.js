25d50a06da8b8d4f86a14f1935d2104cdf673ac6
Roc Tanweer <mohammadtanweer769@gmail.com>
2024-02-21 23:35:44 +0530

    ADD ➕: thunk and reducer function in usersSlice
    This includes implementation of async thunk, reducer,
    handling action events, and selector function related
    to updating the role of a user

--- client/src/state/slices/usersSlice.js
+++ client/src/state/slices/usersSlice.js
@@ -33,6 +33,22 @@ export const usersCountAsync = createAsyncThunk(
   }
 );
 
+export const usersRoleUpdateAsync = createAsyncThunk(
+  "users/usersRoleUpdate",
+  async ({ userId, role }, { rejectWithValue }) => {
+    try {
+      const response = await axCli.patch(`/api/admin/users/${userId}`, {
+        role,
+      });
+      return response.data;
+    } catch (error) {
+      if (error.response) {
+        return rejectWithValue(error.response.data);
+      }
+    }
+  }
+);
+
 export const usersSlice = createSlice({
   name: "users",
   initialState: {
@@ -41,9 +57,20 @@ export const usersSlice = createSlice({
     actions: {
       fetchUsersList: { status: "idle", error: null },
       fetchUsersCount: { status: "idle", error: null },
+      changeUsersRole: { status: "idle", error: null },
     },
   },
-  reducers: {},
+  reducers: {
+    updateUserRole: (state, action) => {
+      state.usersList = state.usersList.map((user) => {
+        const { userId, role } = action.payload;
+        if (user.user_id === userId) {
+          return { ...user, role: role };
+        }
+        return user;
+      });
+    },
+  },
   extraReducers: (builder) => {
     builder
       .addCase(usersListAsync.pending, (state) => {
@@ -67,6 +94,16 @@ export const usersSlice = createSlice({
       .addCase(usersCountAsync.rejected, (state, action) => {
         state.actions.fetchUsersCount.status = "failed";
         state.actions.fetchUsersCount.error = action.payload;
+      })
+      .addCase(usersRoleUpdateAsync.pending, (state) => {
+        state.actions.changeUsersRole.status = "loading";
+      })
+      .addCase(usersRoleUpdateAsync.fulfilled, (state) => {
+        state.actions.changeUsersRole.status = "succeeded";
+      })
+      .addCase(usersRoleUpdateAsync.rejected, (state, action) => {
+        state.actions.changeUsersRole.status = "failed";
+        state.actions.changeUsersRole.error = action.payload;
       });
   },
 });
@@ -77,5 +114,7 @@ export const usersListStatusSelector = (state) =>
   state.users.actions.fetchUsersList.status;
 export const usersCountStatusSelector = (state) =>
   state.users.actions.fetchUsersCount.status;
+
+export const { updateUserRole } = usersSlice.actions;
 
 export default usersSlice.reducer;
