547a0ae540efbb253788d6f8e1da9b69544a7372
Roc Tanweer <mohammadtanweer769@gmail.com>
2024-02-21 23:43:05 +0530

    MOD 🖊️: Extracted Row and Pagination component
    After implementing separate component for usersTable
    row and pagination, I added them on this layout section

--- client/src/pages/admin/layouts/Users.jsx
+++ client/src/pages/admin/layouts/Users.jsx
@@ -2,21 +2,13 @@ import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import {
-  Menu,
-  MenuItem,
-  TableContainer,
-  Typography,
   Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TablePagination,
   TableRow,
+  TableCell,
+  TableBody,
+  TableHead,
   TableFooter,
-  Button,
-  Chip,
-  Box,
-  Avatar,
+  TableContainer,
 } from "@mui/material";
 
 import {
@@ -28,52 +20,16 @@ import {
   usersCountStatusSelector,
 } from "../../../state/slices/usersSlice";
 
-import { FlexBox } from "../../../layouts";
-import { TableLoading } from "../components";
+import { TableLoading, UsersTableRow, CustomPagination } from "../components";
 import { ROWS_PER_PAGE, userTableCols } from "../constants/tablesData";
 
 const Users = () => {
-  const [anchorEl, setAnchorEl] = useState(null);
-
-  const open = Boolean(anchorEl);
-
-  const handleChangeRole = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleCloseRoleMenu = (role) => {
-    setAnchorEl(null);
-    console.log(role);
-  };
-
   const [page, setPage] = useState(0);
-  const [latestPage, setLatestPage] = useState(0);
   const dispatch = useDispatch();
   const usersList = useSelector(usersListSelector);
-  const visibleRows = usersList.slice(
-    page * ROWS_PER_PAGE,
-    (page + 1) * ROWS_PER_PAGE
-  );
   const usersCount = useSelector(usersCountSelector);
   const usersListStatus = useSelector(usersListStatusSelector);
   const usersCountStatus = useSelector(usersCountStatusSelector);
-
-  const handleChangePage = async (event, newPage) => {
-    try {
-      if (newPage > page && page === latestPage) {
-        await dispatch(
-          usersListAsync({ page: newPage + 1, pageSize: ROWS_PER_PAGE })
-        ).unwrap();
-
-        setLatestPage(newPage);
-        setPage(newPage);
-      }
-      setPage(newPage);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   const isMounted = useRef(true);
 
   useEffect(() => {
@@ -99,6 +55,11 @@ const Users = () => {
     getUsers();
   }, [dispatch]);
 
+  const visibleRows = usersList.slice(
+    page * ROWS_PER_PAGE,
+    (page + 1) * ROWS_PER_PAGE
+  );
+
   return (
     <TableContainer
       sx={{ maxWidth: "500px", width: "100%", minHeight: "500px" }}
@@ -117,54 +78,17 @@ const Users = () => {
           )}
           {usersListStatus === "succeeded" &&
             visibleRows.map((user) => (
-              <TableRow key={user.user_id}>
-                <TableCell>{user.user_id}</TableCell>
-                <TableCell>
-                  <FlexBox csx={{ justifyContent: "flex-start", gap: 1 }}>
-                    <Box>
-                      <Avatar alt={user.name} src="/papa" />
-                    </Box>
-                    <Box>
-                      <Typography variant="body2">{user.name}</Typography>
-                      <Typography variant="subtitle" color={"textSecondary"}>
-                        {user.email}
-                      </Typography>
-                    </Box>
-                  </FlexBox>
-                </TableCell>
-                <TableCell
-                  sx={(theme) => {
-                    return {
-                      color: theme.palette.primary.main,
-                    };
-                  }}
-                >
-                  <Button onClick={handleChangeRole}>
-                    <Chip label={user.role} />
-                  </Button>
-                  <Menu
-                    anchorEl={anchorEl}
-                    open={open}
-                    onClose={() => handleCloseRoleMenu("USER")}
-                  >
-                    <MenuItem onClick={() => handleCloseRoleMenu("ADMIN")}>
-                      ADMIN
-                    </MenuItem>
-                  </Menu>
-                </TableCell>
-              </TableRow>
+              <UsersTableRow key={user.user_id} user={user} />
             ))}
         </TableBody>
         <TableFooter>
           <TableRow>
             {usersCountStatus === "succeeded" && (
-              <TablePagination
-                rowsPerPageOptions={[]}
-                colSpan={3}
-                count={usersCount}
-                rowsPerPage={ROWS_PER_PAGE}
+              <CustomPagination
                 page={page}
-                onPageChange={handleChangePage}
+                setPage={setPage}
+                totalRows={usersCount}
+                handleFetchData={usersListAsync}
               />
             )}
           </TableRow>
