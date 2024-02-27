1da25f308981eccf27c423024e392f60204357dd
Roc Tanweer <mohammadtanweer769@gmail.com>
2024-02-28 01:41:00 +0530

    ADD ➕: util func to get modified key-values pairs
    It takes two objects with same structure: original and modified
    Then it returns a new object such that for same keys in original and
    modified object, values are different

--- client/src/utils/function.js
+++ client/src/utils/function.js
@@ -3,3 +3,10 @@ import Cookies from "js-cookie";
 export const isLoggedIn = () => {
   return Boolean(Cookies.get("qcticket"));
 };
+export const filterKeyValuePair = (obj1, obj2) => {
+  const filteredObj = Object.entries(obj1).filter(([key1, value1]) => {
+    return obj2[key1] !== value1;
+  });
+  const newObj = Object.fromEntries(filteredObj);
+  return newObj;
+};
