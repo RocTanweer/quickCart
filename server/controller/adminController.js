d5df0a1d3928e7f112edd6645f6b2c5927466516
Roc Tanweer <mohammadtanweer769@gmail.com>
2024-02-28 01:37:16 +0530

    ADD ➕: controller functions on ProductCategory
    It also handles CRUD operations

--- server/controller/adminController.js
+++ server/controller/adminController.js
@@ -1,4 +1,11 @@
 import {
+  createProductCategory,
+  deleteProductCategory,
+  getProductCategories,
+  getProductCategoriesCount,
+  updateProductCategory,
+} from "../database/productCategoryQueries.js";
+import {
   deleteUser,
   getUsers,
   getUsersCount,
@@ -43,6 +50,57 @@ export const removeUser = async (req, res) => {
     const userId = req.params.userId;
 
     await deleteUser(userId);
+    res.sendStatus(204);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const addProductCategory = async (req, res) => {
+  try {
+    const data = req.body;
+    await createProductCategory(data);
+
+    res.sendStatus(201);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const productCategoryList = async (req, res) => {
+  try {
+    const config = req.query;
+    const productCategoriesList = await getProductCategories(config);
+    res.status(200).json({ productCategoriesList });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const productCategoriesCount = async (req, res) => {
+  try {
+    const productCategoriesCount = await getProductCategoriesCount();
+    res.status(200).json({ count: productCategoriesCount });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const editProductCategory = async (req, res) => {
+  try {
+    const productCategoryId = req.params.productCategoryId;
+    const updateDetails = req.body;
+    await updateProductCategory(productCategoryId, updateDetails);
+    res.sendStatus(204);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const removeProductCategory = async (req, res) => {
+  try {
+    const productCategoryId = req.params.productCategoryId;
+    await deleteProductCategory(productCategoryId);
     res.sendStatus(204);
   } catch (error) {
     res.status(500).json({ message: error.message });
