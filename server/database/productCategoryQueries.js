a8e68b2c6c846975035c9bb70d9b624a5eb02d53
Roc Tanweer <mohammadtanweer769@gmail.com>
2024-02-28 01:35:23 +0530

    ADD ➕: query functions related to productCategories
    These functions performs CRUD operations

---
+++ server/database/productCategoryQueries.js
@@ -0,0 +1,67 @@
+import { connection } from "../config/database.js";
+
+export const createProductCategory = async (data) => {
+  try {
+    const sql = `INSERT INTO product_category
+            (name, thumbnail) VALUES (?, ?)
+        `;
+    const values = [data.name, data.thumbnail];
+    await connection.execute(sql, values);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const getProductCategories = async (config) => {
+  try {
+    const { offset, rowsCount } = config;
+    const sql = `SELECT * FROM product_category LIMIT ?, ?`;
+    const values = [offset, rowsCount];
+    const [results] = await connection.execute(sql, values);
+    return results;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getProductCategoriesCount = async () => {
+  try {
+    const sql = "SELECT COUNT(*) FROM product_category";
+    const [result] = await connection.execute(sql);
+    return result[0]["COUNT(*)"];
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const updateProductCategory = async (
+  productCategoryId,
+  productCategoryUpdates
+) => {
+  try {
+    const { name = null, thumbnail = null } = productCategoryUpdates;
+
+    const sql = `UPDATE product_category
+            SET
+            name = COALESCE(?, name),
+            thumbnail = COALESCE(?, thumbnail)
+            WHERE id = ?
+        `;
+    const values = [name, thumbnail, productCategoryId];
+
+    await connection.execute(sql, values);
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const deleteProductCategory = async (productCategoryId) => {
+  try {
+    const sql = "DELETE FROM product_category WHERE id = ?";
+    const value = [productCategoryId];
+    await connection.execute(sql, value);
+  } catch (error) {
+    throw error;
+  }
+};
