8a4881309b55429d060cd56bb23a5334d8baeb9c
Roc Tanweer <mohammadtanweer769@gmail.com>
2024-02-28 23:52:04 +0530

    MOVED: all user-related routes from authRoutes
    This is done for better file organization.
    I also removed /admin prepended strings from them.

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,29 @@
-import { getUserData } from "../controller/userController.js";
-import { checkForAuthorizationToken } from "../middleware/auth.js";
+import {
+  getUserData,
+  removeUser,
+  updateUserData,
+  userList,
+  usersCount,
+} from "../controller/userController.js";
+import {
+  checkForAuthorizationToken,
+  checkForAdminRole,
+} from "../middleware/auth.js";
 
 export default (router) => {
+  router.get("/users", checkForAuthorizationToken, checkForAdminRole, userList);
+  router.get(
+    "/users/count",
+    checkForAuthorizationToken,
+    checkForAdminRole,
+    usersCount
+  );
   router.get("/users/:userId", checkForAuthorizationToken, getUserData);
+  router.patch("/users/:userId", checkForAuthorizationToken, updateUserData);
+  router.delete(
+    "/users/:userId",
+    checkForAuthorizationToken,
+    checkForAdminRole,
+    removeUser
+  );
 };
