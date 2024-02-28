7679d5ccfc94951a506e488c856b5a2590b05ce7
Roc Tanweer <mohammadtanweer769@gmail.com>
2024-02-28 23:55:44 +0530

    MOVED: all functions from authController to here
    I did it for better file organizations

---
+++ server/controller/productCategoryController.js
@@ -0,0 +1,47 @@
+import {
+  createProductCategory,
+  getProductCategories,
+  getProductCategoriesCount,
+  updateProductCategory,
+} from "../database/productCategoryQueries.js";
+
+export const addProductCategory = async (req, res) => {
+  try {
+    const data = req.body;
+    await createProductCategory(data);
+
+    res.sendStatus(201);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const productCategoryList = async (req, res) => {
+  try {
+    const config = req.query;
+    const productCategoriesList = await getProductCategories(config);
+    res.status(200).json({ productCategoriesList });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const productCategoriesCount = async (req, res) => {
+  try {
+    const productCategoriesCount = await getProductCategoriesCount();
+    res.status(200).json({ count: productCategoriesCount });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const editProductCategory = async (req, res) => {
+  try {
+    const productCategoryId = req.params.productCategoryId;
+    const updateDetails = req.body;
+    await updateProductCategory(productCategoryId, updateDetails);
+    res.sendStatus(204);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
