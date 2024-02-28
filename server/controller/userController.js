31c86d417113a0368481164467531c478bd552b4
Roc Tanweer <mohammadtanweer769@gmail.com>
2024-02-28 23:47:36 +0530

    ADD/MV: controller func to modify user entity
    updateUserDate func will handle any type of modification
    on the user entity.
    I moved user related controller func from authController to here

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,4 +1,30 @@
-import { getUserById } from "../database/userQueries.js";
+import {
+  getUserById,
+  getUsers,
+  getUsersCount,
+  updateUser,
+  deleteUser,
+} from "../database/userQueries.js";
+
+export const userList = async (req, res) => {
+  try {
+    const config = req.query;
+
+    const users = await getUsers(config);
+    res.status(200).json({ users });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const usersCount = async (req, res) => {
+  try {
+    const usersCount = await getUsersCount();
+    res.status(200).json({ count: usersCount });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 export const getUserData = async (req, res) => {
   try {
@@ -20,5 +46,24 @@ export const getUserData = async (req, res) => {
 
 export const updateUserData = async (req, res) => {
   try {
-  } catch (error) {}
+    const userId = req.params.userId;
+    const updateDetails = req.body;
+
+    await updateUser(userId, updateDetails);
+
+    res.sendStatus(204);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const removeUser = async (req, res) => {
+  try {
+    const userId = req.params.userId;
+
+    await deleteUser(userId);
+    res.sendStatus(204);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
