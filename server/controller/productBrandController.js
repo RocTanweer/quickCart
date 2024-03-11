35ce08c24f67adec597e1ed76425e35a11e29372
Roc Tanweer <mohammadtanweer769@gmail.com>
2024-03-12 00:38:59 +0530

    ADD ➕: controller functions for pBrand queries
    These functions handles incoming client requests

---
+++ server/controller/productBrandController.js
@@ -0,0 +1,47 @@
+import {
+  createProductBrand,
+  getProductBrands,
+  getProductBrandsCount,
+  updateProductBrand,
+} from "../database/productBrandQueries.js";
+
+export const addProductBrand = async (req, res) => {
+  try {
+    const data = req.body;
+    await createProductBrand(data);
+
+    res.sendStatus(201);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const productBrandList = async (req, res) => {
+  try {
+    const config = req.query;
+    const brandList = await getProductBrands(config);
+    res.status(200).json({ brandList });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const productBrandsCount = async (req, res) => {
+  try {
+    const brandsCount = await getProductBrandsCount();
+    res.status(200).json({ count: brandsCount });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const editProductBrand = async (req, res) => {
+  try {
+    const productBrandId = req.params.productBrandId;
+    const updateDetails = req.body;
+    await updateProductBrand(productBrandId, updateDetails);
+    res.sendStatus(204);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
