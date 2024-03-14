03e215b9dab1db3a6645430346faa311c48c90fd
Roc Tanweer <mohammadtanweer769@gmail.com>
2024-03-15 02:36:09 +0530

    ADD ➕: form validation schema for new product frm

--- client/src/lib/yupSchemas.js
+++ client/src/lib/yupSchemas.js
@@ -1,5 +1,10 @@
-import { object, string } from "yup";
+import { object, string, number } from "yup";
 
 export const emailValidator = object({
   email: string().email("Invalid email"),
+});
+
+export const newProductFormValSch = object({
+  unitPrice: number().positive("Unit price cannot be 0"),
+  image: string().required("Please choose an image"),
 });
