e3ce7a64d3dd41668d365a93d7e868a3f458d156
Roc Tanweer <mohammadtanweer769@gmail.com>
2024-03-19 02:05:34 +0530

    ADD ➕: column names for product table on admin pg

--- client/src/pages/admin/constants/tablesData.js
+++ client/src/pages/admin/constants/tablesData.js
@@ -2,3 +2,13 @@ export const ROWS_PER_PAGE = 3;
 export const userTableCols = ["Id", "Name", "Role"];
 export const productCategoryTableCols = ["Id", "Thumbnail", "Name"];
 export const productBrandTableCols = ["Id", "Name"];
+export const productsTableCols = [
+  "Id",
+  "Image",
+  "Name",
+  "Description",
+  "Unit Price",
+  "Stock Quantity",
+  "Category",
+  "Brand",
+];
