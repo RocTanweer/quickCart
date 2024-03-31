77d2eb45040c41dfb38238f30f83680d39779e8a
Roc Tanweer <mohammadtanweer769@gmail.com>
2024-03-31 21:21:40 +0530

    ADD ➕: rendering product items on products page
    This only includes the UI for product items in a 9x9 grid

--- client/src/pages/admin/constants/tablesData.js
+++ client/src/pages/admin/constants/tablesData.js
@@ -1,4 +1,4 @@
-export const ROWS_PER_PAGE = 3;
+export const ROWS_PER_PAGE = 5;
 export const userTableCols = ["Id", "Name", "Role"];
 export const productCategoryTableCols = ["Id", "Thumbnail", "Name"];
 export const productBrandTableCols = ["Id", "Name"];
--- client/src/pages/products/Products.jsx
+++ client/src/pages/products/Products.jsx
@@ -1,5 +1,123 @@
+import { useEffect, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import {
+  Box,
+  Grid,
+  Card,
+  CardContent,
+  CardMedia,
+  Button,
+  CardActionArea,
+  CardActions,
+  Typography,
+} from "@mui/material";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+
+import { cld } from "../../lib/cloudinaryInstance";
+import { fill } from "@cloudinary/url-gen/actions/resize";
+
+import { FlexBox } from "../../layouts";
+import {
+  PRODUCT_ITEMS_PER_PAGE,
+  productsPublicAsync,
+  productsPublicSelector,
+  productsPublicStatusSelector,
+} from "../../state/slices/productsPublicSlice";
+
 const Products = () => {
-  return <div>Products</div>;
+  const dispatch = useDispatch();
+  const products = useSelector(productsPublicSelector);
+  const productsStatus = useSelector(productsPublicStatusSelector);
+
+  const isMounted = useRef(true);
+
+  useEffect(() => {
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        if (isMounted.current && products.length === 0) {
+          await dispatch(
+            productsPublicAsync({
+              offset: 0,
+              rowsCount: PRODUCT_ITEMS_PER_PAGE,
+            })
+          ).unwrap();
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchProducts();
+  }, [dispatch, products]);
+
+  return (
+    <Box sx={{ maxWidth: "1000px", mx: "auto", mt: "50px" }}>
+      <Grid container columnSpacing={2}>
+        <Grid item md={3} sx={{ border: "2px solid" }}></Grid>
+        <Grid item md={9}>
+          <FlexBox csx={{ justifyContent: "flex-end" }}>
+            <p>Sort By</p>
+          </FlexBox>
+          <Grid container columnSpacing={2} rowSpacing={4}>
+            {(productsStatus === "idle" || productsStatus === "loading") && (
+              <Typography>Loading...</Typography>
+            )}
+            {productsStatus === "succeeded" &&
+              products.map((product) => (
+                <Grid item md={4} key={product.id}>
+                  <Card>
+                    <CardActionArea>
+                      <CardMedia
+                        component="img"
+                        image={cld
+                          .image(product.image)
+                          .resize(fill().width(250).height(220))
+                          .toURL()}
+                        height={"220px"}
+                      />
+                      <CardContent>
+                        <Typography
+                          gutterBottom
+                          variant="h6"
+                          component="div"
+                          sx={{ height: "64px" }}
+                        >
+                          {product.name.length > 34
+                            ? `${product.name.slice(0, 32)}...`
+                            : product.name}
+                        </Typography>
+                      </CardContent>
+                    </CardActionArea>
+                    <CardActions sx={{ justifyContent: "space-between" }}>
+                      <Typography sx={{ fontWeight: "bold" }}>
+                        <sup>&#x20B9;</sup>
+                        {product.unit_price}
+                      </Typography>
+                      <Button
+                        size="small"
+                        variant="outlined"
+                        endIcon={<AddShoppingCartIcon />}
+                      >
+                        Add
+                      </Button>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              ))}
+          </Grid>
+          <FlexBox csx={{ justifyContent: "flex-end" }}>
+            <p>Pagination</p>
+          </FlexBox>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 };
 
 export default Products;
