7ca68f3b0bcb0f1ea94e793d2b7f0d52c6f70d4b
Tanweer <mohammadtanweer769@gmail.com>
2024-04-03 09:52:19 +0530

    ADD: UI and data fetching for filtering process

--- client/src/pages/products/Products.jsx
+++ client/src/pages/products/Products.jsx
@@ -12,6 +12,10 @@ import {
   CardActions,
   Typography,
   IconButton,
+  Checkbox,
+  FormGroup,
+  FormControlLabel,
+  Slider,
 } from "@mui/material";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
@@ -22,20 +26,42 @@ import { fill } from "@cloudinary/url-gen/actions/resize";
 
 import { FlexBox } from "../../layouts";
 import {
+  addFilter,
   PRODUCT_ITEMS_PER_PAGE,
   productsCountSelector,
+  productsFilter,
   productsPublicAsync,
   productsPublicSelector,
   productsPublicStatusSelector,
+  removeFilter,
+  resetState,
 } from "../../state/slices/productsPublicSlice";
+import { axCli } from "../../lib/axiosClient";
+
+const generateQueryString = (obj) => {
+  let qs = "";
+  Object.entries(obj).forEach(([key, value]) => {
+    if (Array.isArray(value)) {
+      value.forEach((val) => {
+        qs += `&${key}=${val}`;
+      });
+    } else {
+      qs += `&${key}=${value}`;
+    }
+  });
+  return qs;
+};
 
 const Products = () => {
   const [page, setPage] = useState(1);
+  const [productCategories, setProductCategories] = useState([]);
+  const [productBrands, setProductBrands] = useState([]);
   const dispatch = useDispatch();
   const products = useSelector(productsPublicSelector);
   const productsCount = useSelector(productsCountSelector);
   const productsStatus = useSelector(productsPublicStatusSelector);
-
+  const filters = useSelector(productsFilter);
+  const [initFilters, setInitFilters] = useState(filters);
   const isMounted = useRef(true);
 
   useEffect(() => {
@@ -52,6 +78,7 @@ const Products = () => {
             productsPublicAsync({
               offset: 0,
               rowsCount: PRODUCT_ITEMS_PER_PAGE,
+              filters: "",
             })
           ).unwrap();
         }
@@ -61,6 +88,32 @@ const Products = () => {
     };
     fetchProducts();
   }, [dispatch, products]);
+
+  useEffect(() => {
+    const fetchProductCategories = async () => {
+      try {
+        const response = await axCli.get("/api/productCategories");
+        setProductCategories((prev) => [
+          ...prev,
+          ...response.data.productCategoriesList,
+        ]);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    const fetchProductBrands = async () => {
+      try {
+        const response = await axCli.get("/api/productBrands");
+        setProductBrands((prev) => [...prev, ...response.data.brandList]);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    if (isMounted.current) {
+      fetchProductCategories();
+      fetchProductBrands();
+    }
+  }, []);
 
   const handleLftPgBtn = () => {
     setPage((prev) => prev - 1);
@@ -72,10 +125,12 @@ const Products = () => {
         (page + 1) * PRODUCT_ITEMS_PER_PAGE > products.length &&
         products.length !== productsCount
       ) {
+        const qs = generateQueryString(filters);
         await dispatch(
           productsPublicAsync({
             offset: products.length,
             rowsCount: PRODUCT_ITEMS_PER_PAGE,
+            filters: qs,
           })
         ).unwrap();
       }
@@ -85,6 +140,54 @@ const Products = () => {
     }
   };
 
+  const handlePriceRange = (event, newValue, activeThumb) => {
+    if (activeThumb === 0) {
+      dispatch(addFilter({ filterType: "minPrice", value: newValue[0] }));
+    } else {
+      dispatch(addFilter({ filterType: "maxPrice", value: newValue[1] }));
+    }
+  };
+
+  const handleFilterBoxes = (event, value) => {
+    if (event.target.checked) {
+      dispatch(
+        addFilter({
+          filterType: event.target.dataset.filtertype,
+          value,
+        })
+      );
+    } else {
+      dispatch(
+        removeFilter({ filterType: event.target.dataset.filtertype, value })
+      );
+    }
+  };
+  const handleApplyFilter = async () => {
+    try {
+      const qs = generateQueryString(filters);
+      dispatch(resetState());
+      await dispatch(
+        productsPublicAsync({
+          offset: 0,
+          rowsCount: PRODUCT_ITEMS_PER_PAGE,
+          filters: qs,
+        })
+      ).unwrap();
+      setInitFilters(filters);
+      setPage(1);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const formatValueLabel = (value) => {
+    // Format the value with currency symbol
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "INR",
+    }).format(value);
+  };
+
   const visibleProducts = products.slice(
     (page - 1) * PRODUCT_ITEMS_PER_PAGE,
     page * PRODUCT_ITEMS_PER_PAGE
@@ -92,8 +195,110 @@ const Products = () => {
 
   return (
     <Box sx={{ maxWidth: "1000px", mx: "auto", mt: "50px" }}>
-      <Grid container columnSpacing={2}>
-        <Grid item md={3} sx={{ border: "2px solid" }}></Grid>
+      <Grid container>
+        <Grid component={"form"} item md={3} sx={{ pl: "20px", pr: "20px" }}>
+          <Box mb={2}>
+            <Typography variant="h7" sx={{ fontWeight: "bold" }}>
+              Product Categories
+            </Typography>
+            {productCategories &&
+              productCategories.map((pc) => (
+                <FormGroup key={pc.id}>
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        size="small"
+                        inputProps={{ "data-filtertype": "productCategories" }}
+                        checked={filters.productCategories.includes(pc.id)}
+                        onChange={(e) => handleFilterBoxes(e, pc.id)}
+                      />
+                    }
+                    label={pc.name}
+                    labelPlacement="start"
+                    sx={{
+                      justifyContent: "space-between",
+                      ml: 0,
+                    }}
+                  />
+                </FormGroup>
+              ))}
+          </Box>
+          <Box mb={2}>
+            <Typography variant="h7" sx={{ fontWeight: "bold" }}>
+              Product Brands
+            </Typography>
+            {productBrands &&
+              productBrands.map((pb) => (
+                <FormGroup key={pb.id}>
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        size="small"
+                        inputProps={{ "data-filtertype": "productBrands" }}
+                        checked={filters.productBrands.includes(pb.id)}
+                        onChange={(e) => handleFilterBoxes(e, pb.id)}
+                      />
+                    }
+                    label={pb.name}
+                    labelPlacement="start"
+                    sx={{
+                      justifyContent: "space-between",
+                      ml: 0,
+                    }}
+                  />
+                </FormGroup>
+              ))}
+          </Box>
+          <Box mb={2}>
+            <Typography variant="h7" sx={{ fontWeight: "bold" }}>
+              Price Range
+            </Typography>
+            <Slider
+              getAriaLabel={() => "Minimum distance"}
+              value={[filters.minPrice, filters.maxPrice]}
+              onChange={handlePriceRange}
+              valueLabelDisplay="auto"
+              disableSwap
+              min={0}
+              max={500000}
+              step={1000}
+              valueLabelFormat={formatValueLabel}
+            />
+          </Box>
+          <Box mb={3}>
+            <Typography variant="h7" sx={{ fontWeight: "bold" }}>
+              Availability
+            </Typography>
+            <FormGroup>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    size="small"
+                    inputProps={{ "data-filtertype": "availability" }}
+                    checked={Boolean(filters.availability)}
+                    onChange={(e) => handleFilterBoxes(e, "available")}
+                  />
+                }
+                label={"Exclude out of stock"}
+                labelPlacement="start"
+                sx={{
+                  justifyContent: "space-between",
+                  ml: 0,
+                }}
+              />
+            </FormGroup>
+          </Box>
+          <FlexBox csx={{ justifyContent: "flex-end" }}>
+            <Button
+              variant="contained"
+              fullWidth
+              onClick={handleApplyFilter}
+              disabled={JSON.stringify(initFilters) === JSON.stringify(filters)}
+            >
+              Apply
+            </Button>
+          </FlexBox>
+        </Grid>
         <Grid item md={9}>
           <FlexBox csx={{ justifyContent: "flex-end" }}>
             <p>Sort By</p>
