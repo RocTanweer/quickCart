3b2773ae3b190194336045976df3ce62af50cbe8
Tanweer <mohammadtanweer769@gmail.com>
2024-04-22 16:46:59 +0530

    ADD ➕: query function to get id of shpcart

--- server/database/shoppingCartItemQueries.js
+++ server/database/shoppingCartItemQueries.js
@@ -17,6 +17,7 @@ export const getShoppingCartItems = async (shoppingCartId) => {
 
 export const addShoppingCartItem = async ({ shoppingCartId, productId }) => {
   try {
+    console.log(shoppingCartId, productId);
     const sql = `INSERT INTO shopping_cart_item (shopping_cart_id, product_id) VALUES (?, ?)`;
     await connection.execute(sql, [shoppingCartId, productId]);
   } catch (error) {
@@ -28,6 +29,16 @@ export const deleteShoppingCartItem = async (shoppingCartItemId) => {
   try {
     const sql = `DELETE FROM shopping_cart_item WHERE id = ?`;
     await connection.execute(sql, [shoppingCartItemId]);
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getShoppingCartId = async (userId) => {
+  try {
+    const sql = `SELECT id from shopping_cart WHERE user_id = ?`;
+    const [result] = await connection.execute(sql, [userId]);
+    return result[0].id;
   } catch (error) {
     throw error;
   }
