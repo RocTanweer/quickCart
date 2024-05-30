d3e3f0c8aa664c729cea2af9dddecd90aafc7fc3
Tanweer <mohammadtanweer769@gmail.com>
2024-05-31 00:05:27 +0530

    ADD ➕: client url for CORS purposes

--- server/config/envVar.js
+++ server/config/envVar.js
@@ -17,3 +17,5 @@ export const SERVER_PORT = process.env.PORT || 4000;
 export const CA_CERT = process.env.CA_CERT.replace(/\\n/g, "\n");
 
 export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
+
+export const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:5173";
