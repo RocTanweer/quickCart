11456c26ef209cfb05608340a82765cbaa4b23cc
Tanweer <mohammadtanweer769@gmail.com>
2025-09-25 16:05:29 +0530

    ADD ➕: header and basic form UI on checkout page

--- client/src/pages/checkout/Checkout.jsx
+++ client/src/pages/checkout/Checkout.jsx
@@ -1,5 +1,150 @@
+import {
+  Box,
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+
+import CheckoutHeader from "./components/CheckoutHeader";
+import { FlexBox } from "../../layouts";
+
+import { states } from "./constants/states";
+
 const Checkout = () => {
-  return <div>Checkout</div>;
+  return (
+    <Box sx={{ minHeight: "100vh" }}>
+      <CheckoutHeader />
+      <Box sx={{ maxWidth: "700px", mx: "auto", mt: "50px" }}>
+        <Box
+          sx={{
+            border: "1px solid",
+            borderRadius: "10px",
+            borderColor: (theme) => theme.palette.grey[400],
+            padding: "30px",
+          }}
+        >
+          <Typography variant="h4" sx={{ mb: 2 }}>
+            Address
+          </Typography>
+
+          <Stack component="form" spacing={2} onSubmit={null}>
+            <Box>
+              <TextField
+                label="Receiver Name"
+                type="text"
+                name="name"
+                fullWidth
+                required
+                autoComplete="off"
+              />
+            </Box>
+
+            <FlexBox csx={{ gap: 2 }}>
+              <TextField
+                label="Mobile"
+                name="mobile"
+                type="text"
+                fullWidth
+                required
+                autoComplete="off"
+                inputProps={{
+                  inputMode: "numeric",
+                  maxLength: "10",
+                  pattern: "\\d*",
+                }}
+                onInput={(e) => {
+                  e.target.value = e.target.value
+                    .replace(/\D/g, "")
+                    .slice(0, 10);
+                }}
+              />
+
+              <TextField
+                label="PIN code"
+                name="pin"
+                type="text"
+                fullWidth
+                required
+                autoComplete="off"
+                inputProps={{
+                  inputMode: "numeric",
+                  maxLength: "6",
+                  pattern: "\\d*",
+                }}
+                onInput={(e) => {
+                  e.target.value = e.target.value
+                    .replace(/\D/g, "")
+                    .slice(0, 6);
+                }}
+              />
+            </FlexBox>
+
+            <TextField
+              label="House/Apartment/Colony/Street"
+              type="text"
+              name="addressLine1"
+              fullWidth
+              required
+              autoComplete="off"
+            />
+            <TextField
+              label="Area/Sector/Lane"
+              type="text"
+              name="addressLine2"
+              fullWidth
+              required
+              autoComplete="off"
+            />
+
+            <FormControl fullWidth>
+              <InputLabel required id="landmark-label">
+                Landmark
+              </InputLabel>
+              <Select labelId="landmark-label" id="landmarkId" label="landmark">
+                {states.map((state, i) => (
+                  <MenuItem value={state} key={i}>
+                    {state}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            <FlexBox csx={{ gap: 2 }}>
+              <TextField
+                label="City/Town"
+                name="city"
+                type="text"
+                fullWidth
+                required
+                autoComplete="off"
+              />
+
+              <FormControl fullWidth>
+                <InputLabel required id="state-label">
+                  State
+                </InputLabel>
+                <Select labelId="state-label" id="stateId" label="state">
+                  {states.map((state, i) => (
+                    <MenuItem value={state} key={i}>
+                      {state}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </FlexBox>
+
+            <Button type="submit" variant="contained">
+              Submit
+            </Button>
+          </Stack>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default Checkout;
--- client/src/pages/checkout/components/CheckoutHeader.jsx
+++ client/src/pages/checkout/components/CheckoutHeader.jsx
@@ -1,5 +1,37 @@
+import { NavLink } from "react-router-dom";
+
+import { Container, Typography, AppBar, Toolbar, Button } from "@mui/material";
+
+import { FlexBox } from "../../../layouts";
+
 const CheckoutHeader = () => {
-  return <div>CheckoutHeader</div>;
+  return (
+    <AppBar position="sticky">
+      <Container maxWidth="xl" sx={{ mt: "10px" }}>
+        <Toolbar disableGutters>
+          <FlexBox csx={{ width: "100%", justifyContent: "space-between" }}>
+            <Typography
+              variant="h6"
+              noWrap
+              sx={{
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".2rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              QuickCart
+            </Typography>
+            <Typography variant="h3">Checkout</Typography>
+            <Button component={NavLink} to="/cart" variant="contained">
+              Edit Cart
+            </Button>
+          </FlexBox>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 };
 
 export default CheckoutHeader;
